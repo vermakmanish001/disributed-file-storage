b22458648ff460fb334b9206bdd1bd4941ff3fd4
Manish Verma <your.vermakmanish001@egmail.com>
2024-11-27 16:36:01 +0530

    d

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -10,5 +10,5 @@ func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
 
-	assert.Equal(t, tr.listenAddress, listenAddr)
+	assert.Equal(t, tr.ListenAddress(), listenAddr)
 }
